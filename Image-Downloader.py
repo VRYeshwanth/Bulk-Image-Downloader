704160073453f123270ba2d0431fef8965814e69
VR Yeshwanth <vryeshwanth06@gmail.com>
2024-09-03 11:03:22 +0530

    Font style and size

--- Image-Downloader.py
+++ Image-Downloader.py
@@ -3,33 +3,33 @@ import tkinter as tk
 window = tk.Tk()
 window.title('Bulk Image Downloader')
 
-heading = tk.Label(window, text="Bulk Image Downloader")
-heading.pack()
+heading = tk.Label(window, text="Bulk Image Downloader", font=("Comic Sans MS",20,"bold"))
+heading.pack(padx=20, pady=15)
 
 inp_frame = tk.Frame(window)
 inp_frame.pack()
 
-f_text = tk.Label(inp_frame, text="Input File :")
+f_text = tk.Label(inp_frame, text="Input File :", font=("Comic Sans MS",15))
 f_text.grid(row=0, column=0)
 
 f1 = tk.StringVar()
-f_ent = tk.Entry(inp_frame, textvariable=f1)
+f_ent = tk.Entry(inp_frame, textvariable=f1, font=("Comic Sans MS",13))
 f_ent.grid(row=0, column=1)
 
-f_btn = tk.Button(inp_frame, text="Browse")
+f_btn = tk.Button(inp_frame, text="Browse", font=("Comic Sans MS",15))
 f_btn.grid(row=0, column=2)
 
-fold_text = tk.Label(inp_frame, text="Output Folder :")
+fold_text = tk.Label(inp_frame, text="Output Folder :", font=("Comic Sans MS",15))
 fold_text.grid(row=1, column=0)
 
 f2 = tk.StringVar()
-fold_ent = tk.Entry(inp_frame, textvariable=f2)
+fold_ent = tk.Entry(inp_frame, textvariable=f2, font=("Comic Sans MS",13))
 fold_ent.grid(row=1, column=1)
 
-fold_btn = tk.Button(inp_frame, text="Browse")
+fold_btn = tk.Button(inp_frame, text="Browse", font=("Comic Sans MS",15))
 fold_btn.grid(row=1,column=2)
 
-download_btn = tk.Button(inp_frame, text="Download")
+download_btn = tk.Button(inp_frame, text="Download", font=("Comic Sans MS",15))
 download_btn.grid(row=2, columnspan=3)
 
 window.mainloop()
